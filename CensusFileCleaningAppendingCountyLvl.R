5c94ebcaae5c0af16c5a9d5a9a260243c0ead841
wjtimmerman <wjtimmerman@gmail.com>
2016-02-27 14:46:49 -0500

    Extracting County Census Data into RData File
    Uses files stored on Wylie’s computer

---
+++ CensusFileCleaningAppendingCountyLvl.R
@@ -0,0 +1,45 @@
+#setwd(" ")
+setwd("~/Desktop/hackathon/Census Data/")
+rm(list = ls())
+library("ggplot2")
+library("plyr")
+library("dplyr")
+library("gridExtra")
+#invisible(readline(prompt="Press [enter] to continue"))
+#LOADING DATA (not using loops because i'm too lazy to learn that right now)
+ACS.1.Year.2014.All.Counties <- read.csv("ACS 1-Year 2014 All Counties.csv", header=TRUE)
+ACS.1.Year.2013.All.Counties <- read.csv("ACS 1-Year 2013 All Counties.csv", header=TRUE)
+ACS.1.Year.2012.All.Counties <- read.csv("ACS 1-Year 2012 All Counties.csv", header=TRUE)
+ACS.1.Year.2011.All.Counties <- read.csv("ACS 1-Year 2011 All Counties.csv", header=TRUE)
+ACS.1.Year.2010.All.Counties <- read.csv("ACS 1-Year 2010 All Counties.csv", header=TRUE)
+ACS.1.Year.2009.All.Counties <- read.csv("ACS 1-Year 2009 All Counties.csv", header=TRUE)
+ACS.1.Year.2008.All.Counties <- read.csv("ACS 1-Year 2008 All Counties.csv", header=TRUE)
+#DROPPING EXTRA HEADER ROW FROM EACH TABLE
+ACS.1.Year.2014.All.Counties <- ACS.1.Year.2014.All.Counties[-1,]
+ACS.1.Year.2013.All.Counties <- ACS.1.Year.2013.All.Counties[-1,]
+ACS.1.Year.2012.All.Counties <- ACS.1.Year.2012.All.Counties[-1,]
+ACS.1.Year.2011.All.Counties <- ACS.1.Year.2011.All.Counties[-1,]
+ACS.1.Year.2010.All.Counties <- ACS.1.Year.2010.All.Counties[-1,]
+ACS.1.Year.2009.All.Counties <- ACS.1.Year.2009.All.Counties[-1,]
+ACS.1.Year.2008.All.Counties <- ACS.1.Year.2008.All.Counties[-1,]
+#ADDING IN YEARS TO EACH TABLE
+ACS.1.Year.2014.All.Counties$year <- "2014"
+ACS.1.Year.2013.All.Counties$year <- "2013"
+ACS.1.Year.2012.All.Counties$year <- "2012"
+ACS.1.Year.2011.All.Counties$year <- "2011"
+ACS.1.Year.2010.All.Counties$year <- "2010"
+ACS.1.Year.2009.All.Counties$year <- "2009"
+ACS.1.Year.2008.All.Counties$year <- "2008"
+invisible(readline(prompt="Press [enter] to continue"))
+#SETTING WHAT VARIABLES TO KEEP
+varstokeep <- c("FIPS","year","Name.of.Area","Qualifying.Name","State.U.S..Abbreviation..USPS.","Summary.Level","County","Geographic.Identifier","Area..Land.","Area..Water.","Total.Population","Population.Density..per.sq..mile.")
+ACS.1.Year.2008.All.Counties<- ACS.1.Year.2008.All.Counties[varstokeep]
+ACS.1.Year.2009.All.Counties<- ACS.1.Year.2009.All.Counties[varstokeep]
+ACS.1.Year.2010.All.Counties<- ACS.1.Year.2010.All.Counties[varstokeep]
+ACS.1.Year.2011.All.Counties<- ACS.1.Year.2011.All.Counties[varstokeep]
+ACS.1.Year.2012.All.Counties<- ACS.1.Year.2012.All.Counties[varstokeep]
+ACS.1.Year.2013.All.Counties<- ACS.1.Year.2013.All.Counties[varstokeep]
+ACS.1.Year.2014.All.Counties<- ACS.1.Year.2014.All.Counties[varstokeep]
+#APPENDING DATASETS FOR EACH YEAR TOGETHER
+ACS.1.Year.20082014.All.Counties <- rbind(ACS.1.Year.2014.All.Counties, ACS.1.Year.2013.All.Counties, ACS.1.Year.2012.All.Counties, ACS.1.Year.2011.All.Counties, ACS.1.Year.2010.All.Counties, ACS.1.Year.2009.All.Counties, ACS.1.Year.2008.All.Counties)
+save(ACS.1.Year.20082014.All.Counties, file = "ACS0815.RData")
