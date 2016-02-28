9da825aa9260d074d954d298678a6a0d88341299
Eugene Tsuprun <etsuprun@gmail.com>
2016-02-28 02:50:28 -0500

    Added files via upload

--- CountyBarChartShiny/server.R
+++ CountyBarChartShiny/server.R
@@ -1,10 +1,21 @@
-setwd("~/Desktop/hackathon/uncrashgithub/") #Wylie's directory
 rm(list = ls())
 library(shiny)
 library(ggplot2)
+load("alcohol.Rdata")
 #source("helpers.R")
 #counties <- readRDS("data/counties.rds") #Commented out until I try map thing again
 #loading me data
+<<<<<<< HEAD
+acs2014 <- readRDS("data/ACS2014.rds")
+acs2013 <- readRDS("data/ACS2013.rds")
+acs2012 <- readRDS("data/ACS2012.rds")
+acs2011 <- readRDS("data/ACS2011.rds")
+acs2010 <- readRDS("data/ACS2010.rds")
+acs2009 <- readRDS("data/ACS2009.rds")
+acs2008 <- readRDS("data/ACS2008.rds")
+library(maps)
+library(mapproj)
+=======
 acs2014 <- readRDS("CountyBarChartShiny/data/ACS2014.rds")
 acs2013 <- readRDS("CountyBarChartShiny/data/ACS2013.rds")
 acs2012 <- readRDS("CountyBarChartShiny/data/ACS2012.rds")
@@ -14,6 +25,7 @@ acs2009 <- readRDS("CountyBarChartShiny/data/ACS2009.rds")
 acs2008 <- readRDS("CountyBarChartShiny/data/ACS2008.rds")
 #library(maps)
 #library(mapproj)
+>>>>>>> origin/master
 # Define server logic for slider examples
 shinyServer(function(input, output) {
 
@@ -66,6 +78,40 @@ shinyServer(function(input, output) {
                      "2010" = "2010",
                      "2009" = "2009",
                      "2008" = "2008")
+<<<<<<< HEAD
+    ggplot(data=data, aes(x=`Name.of.Area`, y=`Population.Density..per.sq..mile.`)) +
+      geom_bar(stat="identity") +coord_flip()  + theme_minimal() + labs(title= legend)
+=======
     ggplot(data = data, aes(x=`Name.of.Area`, y=`incdensity`)) + geom_bar(stat="identity") + coord_flip()  + theme_minimal() + labs(title= legend)
+>>>>>>> origin/master
   })
+  
+  output$drunkPlot <- renderPlot ({
+    
+    slices <-       c(alcohol$men[alcohol$year==input$year2  & alcohol$alcohol==input$drunk],
+                      alcohol$women[alcohol$year==input$year2  & alcohol$alcohol==input$drunk])
+    lbls <- c(
+      paste0("men: ",
+            round(slices[1]/(slices[1]+slices[2])*100),"%"),
+      paste0("women: ",
+            round(slices[2]/(slices[1]+slices[2])*100),"%")
+    )
+    pie(slices, labels = lbls, main=
+          paste0("Average age: ",
+            round(alcohol$age[alcohol$year==input$year2 & alcohol$alcohol==input$drunk],2)
+          , " years\n\n",
+          "% of accident drivers who were charged with a DWI: ",
+          round(
+          (alcohol$men[alcohol$year==input$year2  & alcohol$alcohol=="Yes"]+
+            alcohol$women[alcohol$year==input$year2  & alcohol$alcohol=="Yes"])/
+            (alcohol$men[alcohol$year==input$year2  & alcohol$alcohol=="No"]+
+               alcohol$women[alcohol$year==input$year2  & alcohol$alcohol=="No"])*100,2),"%"
+            
+            
+          
+          ))
+  })
+
 })
+
+
--- CountyBarChartShiny/ui.R
+++ CountyBarChartShiny/ui.R
@@ -5,23 +5,39 @@ library(shiny)
 shinyUI(fluidPage(
   
   #  Application title
-  titlePanel("Crashes by County, NJ"),
+  titlePanel("NJ Crash Data Visualization"),
   
   # Sidebar with sliders that demonstrate various available
   # options
   sidebarLayout(
     sidebarPanel(
+      
       helpText("Create bar charts with information from NJDOT and the U.S. Census' American Community Survey"),
       # Simple integer interval
+      conditionalPanel(condition="input.currentTab=='Accidents by County'",
       selectInput("year", label = h3("Select year:"),
-                  choices = c("2008", "2009", "2010", "2011", "2012", "2013", "2014"),
-                 selected = "2014"),
-      textOutput("text1")
+                  choices = c(2008:2014),
+                 selected = "2014")
+      )
+,
+      conditionalPanel(
+        condition="input.currentTab=='Drunk drivers'",
+        selectInput("year2", label = h3("Select year:"),
+                    choices = c(2001:2014),
+                    selected = 2014),
+        selectInput("drunk","Drivers charged with DWI?", choices = c("Yes","No")
+        ))
       ),
     # Show a table summarizing the values entered
     mainPanel(
+      tabsetPanel(id="currentTab",
       #plotOutput("map"), #commenting out my plotOutput
-      plotOutput("barPlot")
+        tabPanel("Accidents by County",plotOutput("barPlot")),
+        tabPanel("Drunk drivers",
+                 plotOutput("drunkPlot"),
+                 htmlOutput("drunkText")
+        )
+      )
     )
   )
 ))
